f2be92817e4553d3132208745b9fed32bd4503ea
Myroslav Martsin <forvaro@gmail.com>
2019-02-12 00:02:19 +0200

    create new page home

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ let login = document.getElementById('login');
 let passw = document.getElementById('passw');
 let submit = document.getElementById('submit');
 
-console.log(login.value)
+let currentLogin = '1234';
+let currentPassword = '1234';
+
+function checkLoginAndPassword() {
+    if( login.value === currentLogin && passw.value === currentPassword ) {
+        location.href = 'home/home.html'
+    } else {
+        alert('Не правелний пароль!')
+    }
+}
+
+submit.addEventListener('click', checkLoginAndPassword);
