94de1b015c3a807cd146d9c1807aa904ce32b24e
Myroslav Martsin <forvaro@gmail.com>
2019-02-12 00:39:33 +0200

    create switch among pages

---
+++ about-us/about-us.js
@@ -0,0 +1,11 @@
+document.getElementById('home').addEventListener('click', function() {
+    location.href = '../home/home.html'
+})
+
+document.getElementById('contact').addEventListener('click', function() {
+    location.href = '../contact/contact.html'
+})
+
+document.getElementById('about-us').addEventListener('click', function() {
+    location.href = '../about-us/about-us.html'
+})
---
+++ contact/contact.js
@@ -0,0 +1,11 @@
+document.getElementById('about-us').addEventListener('click', function() {
+    location.href = '../about-us/about-us.html'
+})
+
+document.getElementById('contact').addEventListener('click', function() {
+    location.href = '../contact/contact.html'
+})
+
+document.getElementById('contact').addEventListener('click', function() {
+    location.href = '../about-us/about-us.html'
+})
